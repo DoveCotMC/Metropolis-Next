f12fdc8c8538aa29a82edf08b9e3574bf60bbaec
Arrokoth <arrokoth233@gmail.com>
2025-08-06 09:55:31 +0200

    How to?

--- src/main/java/team/dovecot/metropolitan/core/common/init/MetroBlocks.java
+++ src/main/java/team/dovecot/metropolitan/core/common/init/MetroBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityT
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -13,7 +14,7 @@ import team.dovecot.metropolitan.core.common.block.BlockTicketingServer;
 import team.dovecot.metropolitan.core.common.block.entity.BlockEntityTicketingServer;
 
 public final class MetroBlocks {
-    public static final Block TICKETING_SERVER = register("ticketing_server", new BlockTicketingServer(BlockBehaviour.Properties.of(Material.METAL)));
+    public static final Block TICKETING_SERVER = register("ticketing_server", new BlockTicketingServer(BlockBehaviour.Properties.of(Material.METAL).strength(8.0f, 3600000.0F)));
 
     private static Block register(String id, Block block) {
         return Registry.register(Registry.BLOCK, new ResourceLocation(Constants.MOD_ID, id), block);
